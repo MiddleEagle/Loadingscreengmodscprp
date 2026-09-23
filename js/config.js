df8ef28ca98b42e99eeb8bfb644f69a3c2ca6797
MiddleEagle <noeouvrard85@gmail.com>
2026-09-23 17:43:18 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 */
 window.LOADING_CONFIG = {
   serverName: "SunLight",
-  serverTagline: "FONDATION SCP — ACCÈS AU SITE 33",
+  serverTagline: "FONDATION SCP — ACCÈS A LA ZONE XX",
   welcomeText: "La Connexion est en cours merci de patienter.",
   playerRole: "Personnel autorisé",
   playerName: "Joueur", // Nom de secours si l'API Steam n'est pas configurée.
@@ -33,16 +33,5 @@ window.LOADING_CONFIG = {
   },
   staff: [
     { name: "DemonTube", grade: "Fondateur", steamId: "76561199466721881", image: "" },
-    { name: "Cacao", grade: "Co Fondateur", steamId: "76561199209366991", image: "" },
-    { name: "Blue", grade: "Co Fondateur", steamId: "76561199245355095", image: "" },
-    { name: "Christ Xiloss", grade: "Gérant Global", steamId: "76561198094185969", image: "" },
-    { name: "MiddleEagle", grade: "Responsable", steamId: "76561199644813006", image: "" },
-    { name: "Zenox", grade: "Administrateur", steamId: "76561199680615624", image: "" },
-    { name: "Leo Epic", grade: "Moderateur Sénior", steamId: "76561199800509330", image: "" },
-    { name: "Jonson", grade: "Moderateur", steamId: "76561199811076597", image: "" },
-    { name: "Asashin", grade: "Moderateur", steamId: "76561199166630728", image: "" },
-    { name: "Pizza", grade: "Modérateur", steamId: "STEAM_0:1:366151153", image: "" },
-    { name: "Zoulou", grade: "Modérateur", steamId: "STEAM_0:1:598195100", image: "" },
-    { name: "Olie", grade: "Modérateur", steamId: "76561199304516443", image: "" },
   ]
 };
